95d5f32c47f1eb3373eea9e2dc9e764a7e325e63
Pavana Radhakrishnamurthy <pavana.radhakrishnamurthy@flex.com>
2024-02-01 22:43:15 +0300

    Changes done to html design

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+faSearch=faSearch;
+}
